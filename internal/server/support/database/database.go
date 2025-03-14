2e89eec2eca7e23e0d3e792de0da5f10d4a8112c
Evgeny Kuvalkin <kuvalkineugene@gmail.com>
2025-03-14 22:53:21 +0300

    migrations for server

---
+++ internal/server/support/database/database.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"embed"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/pressly/goose/v3"
+)
+
+//go:embed migrations/*.sql
+var embedMigrations embed.FS
+
+func InitDB(ctx context.Context, dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("could not connect to database: %w", err)
+	}
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("could not ping database: %w", err)
+	}
+
+	return db, nil
+}
+
+func Migrate(ctx context.Context, db *sql.DB) error {
+	goose.SetBaseFS(embedMigrations)
+
+	if err := goose.SetDialect("postgres"); err != nil {
+		return fmt.Errorf("could not set dialect: %w", err)
+	}
+
+	if err := goose.UpContext(ctx, db, "migrations"); err != nil {
+		return fmt.Errorf("could not migrate database: %w", err)
+	}
+
+	return nil
+}
---
+++ internal/server/support/database/migrations/20250314193532_init.sql
@@ -0,0 +1,23 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    login VARCHAR(250) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    version int8 NOT NULL DEFAULT 1
+);
+CREATE TABLE IF NOT EXISTS entries (
+    user_id UUID REFERENCES users(id) ON DELETE RESTRICT,
+    key TEXT NOT NULL,
+    notes TEXT DEFAULT NULL,
+    is_deleted BOOLEAN DEFAULT FALSE,
+    version int8 NOT NULL DEFAULT 1,
+    user_version int8 NOT NULL,
+    content_name TEXT NOT NULL,
+
+    PRIMARY KEY (user_id, key)
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS entries;
