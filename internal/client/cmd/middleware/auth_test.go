f03df8637a2d4ec8ea6c6ed508d8d5baf3e011a8
Evgeny Kuvalkin <kuvalkineugene@gmail.com>
2025-03-30 18:54:47 +0300

    test: add unit tests for EnsureNotLoggedIn middleware

--- internal/client/cmd/middleware/auth_test.go
+++ internal/client/cmd/middleware/auth_test.go
@@ -108,3 +108,98 @@ func TestEnsureLoggedIn(t *testing.T) {
 		require.Error(t, err)
 	})
 }
+
+func TestEnsureNotLoggedIn(t *testing.T) {
+	ctx, cancel := utils.TestContext(t)
+	defer cancel()
+
+	newTestCommand := func() *cobra.Command {
+		cmd := &cobra.Command{}
+		cmd.SetIn(bytes.NewBuffer(nil))
+		cmd.SetOut(io.Discard)
+		cmd.SetErr(io.Discard)
+		cmd.SetArgs([]string{})
+		cmd.SetContext(ctx)
+		return cmd
+	}
+
+	t.Run("not logged in", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		container := mocks.NewMockContainer(ctrl)
+		auth := mocks.NewMockAuthService(ctrl)
+
+		container.EXPECT().GetAuthService(ctx).Return(auth, nil).AnyTimes()
+		auth.EXPECT().IsLoggedIn(ctx).Return(false, nil)
+
+		mw := middleware.EnsureNotLoggedIn(container)
+		wrapped := mw(nil)
+		err := wrapped(newTestCommand(), []string{})
+		require.NoError(t, err)
+	})
+
+	t.Run("logged in", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		container := mocks.NewMockContainer(ctrl)
+		auth := mocks.NewMockAuthService(ctrl)
+
+		container.EXPECT().GetAuthService(ctx).Return(auth, nil).AnyTimes()
+		auth.EXPECT().IsLoggedIn(ctx).Return(true, nil)
+
+		mw := middleware.EnsureNotLoggedIn(container)
+		wrapped := mw(nil)
+		err := wrapped(newTestCommand(), []string{})
+		require.Error(t, err)
+	})
+
+	t.Run("error getting auth service", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		container := mocks.NewMockContainer(ctrl)
+
+		container.EXPECT().GetAuthService(ctx).Return(nil, errors.New("error getting auth service"))
+
+		mw := middleware.EnsureNotLoggedIn(container)
+		wrapped := mw(nil)
+		err := wrapped(newTestCommand(), []string{})
+		require.Error(t, err)
+	})
+
+	t.Run("error checking if logged in", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		container := mocks.NewMockContainer(ctrl)
+		auth := mocks.NewMockAuthService(ctrl)
+
+		container.EXPECT().GetAuthService(ctx).Return(auth, nil).AnyTimes()
+		auth.EXPECT().IsLoggedIn(ctx).Return(false, errors.New("error checking if logged in"))
+
+		mw := middleware.EnsureNotLoggedIn(container)
+		wrapped := mw(nil)
+		err := wrapped(newTestCommand(), []string{})
+		require.Error(t, err)
+	})
+
+	t.Run("wrapped function error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		container := mocks.NewMockContainer(ctrl)
+		auth := mocks.NewMockAuthService(ctrl)
+
+		container.EXPECT().GetAuthService(ctx).Return(auth, nil).AnyTimes()
+		auth.EXPECT().IsLoggedIn(ctx).Return(false, nil)
+
+		mw := middleware.EnsureNotLoggedIn(container)
+		wrapped := mw(func(cmd *cobra.Command, args []string) error {
+			return errors.New("wrapped function error")
+		})
+		err := wrapped(newTestCommand(), []string{})
+		require.Error(t, err)
+	})
+}
